c8fdc2ef275bfef3a72b43326b67f33ad4cc12e0
ssnell227 <ssnell227@gmail.com>
2020-06-02 14:31:40 -0600

    finished part 1

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express'),
+    app = express(),
+    session = require('express-session'),
+    massive = require('massive'),
+    {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+    authCtrl = require('../server/controllers/authController')
+
+massive({
+    connectionString: CONNECTION_STRING,
+ssl:{rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
+
+
+app.use(express.json())
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
+
+app.post('/auth/register', authCtrl.register)
+
+app.listen(SERVER_PORT, console.log(`Server connected on ${SERVER_PORT}`))
